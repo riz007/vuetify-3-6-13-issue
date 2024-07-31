6044ee4a1454ca9c719c208e2b6b83084ee722f2
riz007 <rizwanulrudra@gmail.com>
2024-07-31 15:44:31 +0700

    feat: vuetify 3.6.13 issue repro

---
+++ src/components/Child.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+export interface IUser {
+  body: string;
+  id: number;
+  title: string;
+  userId: number;
+}
+export interface Props {
+  title: string;
+  isLoading: boolean;
+  data: IUser | null;
+}
+defineProps<Props>();
+</script>
+
+<template>
+  <v-app>
+    <v-container>
+      <v-row>
+        <v-col>
+          <div v-if="isLoading">
+            <v-progress-circular indeterminate></v-progress-circular>
+          </div>
+          <div v-else>
+            <pre>{{ data }}</pre>
+            <h3>Data loaded properly</h3>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
---
+++ src/components/Parent.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { defineAsyncComponent, ref, onMounted } from "vue";
+import axios from "axios";
+import { IUser } from "./Child.vue";
+
+const Child = defineAsyncComponent(() => import("./Child.vue"));
+const isLoading = ref(false);
+const data = ref<IUser | null>(null);
+
+const doSomething = () =>
+  new Promise((resolve, reject) => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts/1")
+      .then((response) => {
+        data.value = response.data;
+        resolve(response.data);
+      })
+      .catch(() => {
+        reject();
+      });
+  });
+
+onMounted(async () => {
+  isLoading.value = true;
+  await doSomething();
+  isLoading.value = false;
+});
+</script>
+
+<template>
+  <h2>Vuetify Axios Issue</h2>
+  <p>
+    If it doesn't work then a circular loader (indeterminate) will appear below:
+  </p>
+  <Child title="Child" :is-loading="isLoading" :data="data" />
+</template>
